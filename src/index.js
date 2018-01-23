baa7f7f4e36a09f4559e214ed419290679e91326
Marcin <emrhinodev@gmail.com>
2018-01-23 16:11:15 +0100

    simple components

--- src/index.js
+++ src/index.js
@@ -1,31 +1,226 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
 
 const data = {
-  title: "temat wpisu",
-  content: "Wpis na blogu, wpis na blogu...",
-  image: "https://placebear.com/300/200",
-  promo: false
+  "history": [
+    {
+      "timestamp": "2017-10-22T00:01:03.532399Z", 
+      "value": 0.138112, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-21T00:00:48.093334Z", 
+      "value": 0.144001, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-20T00:00:35.291838Z", 
+      "value": 0.160873, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-19T00:00:27.536367Z", 
+      "value": 0.172859, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-18T00:01:11.707674Z", 
+      "value": 0.173966, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-17T00:00:53.562298Z", 
+      "value": 0.16121, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-16T00:00:50.780583Z", 
+      "value": 0.16414, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-15T00:00:59.828038Z", 
+      "value": 0.162185, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-14T00:00:50.379697Z", 
+      "value": 0.162968, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-13T00:01:09.508271Z", 
+      "value": 0.161988, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-12T00:00:28.564523Z", 
+      "value": 0.16932, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-11T00:00:41.339802Z", 
+      "value": 0.169617, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-10T00:01:23.832287Z", 
+      "value": 0.168255, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-09T00:00:44.099025Z", 
+      "value": 0.188124, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-08T00:01:28.994764Z", 
+      "value": 0.201153, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-07T00:01:14.232711Z", 
+      "value": 0.209948, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-06T00:00:54.874956Z", 
+      "value": 0.216337, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-05T00:00:57.642396Z", 
+      "value": 0.202121, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-04T00:01:10.830587Z", 
+      "value": 0.210849, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-03T00:00:52.663789Z", 
+      "value": 0.227459, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-02T00:00:41.437222Z", 
+      "value": 0.246748, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-10-01T00:01:21.704451Z", 
+      "value": 0.231678, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-09-30T00:00:57.577299Z", 
+      "value": 0.233209, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-09-29T00:01:03.939776Z", 
+      "value": 0.237252, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-09-28T00:01:00.843966Z", 
+      "value": 0.258216, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-09-27T00:00:28.609724Z", 
+      "value": 0.239122, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-09-26T00:00:35.855857Z", 
+      "value": 0.22051, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-09-25T00:00:35.946223Z", 
+      "value": 0.201783, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-09-24T00:01:01.911773Z", 
+      "value": 0.214196, 
+      "value_currency": "USD", 
+      "when": "3 months ago"
+    }, 
+    {
+      "timestamp": "2017-09-23T00:00:57.238645Z", 
+      "value": 0.204888, 
+      "value_currency": "USD", 
+      "when": "4 months ago"
+    }, 
+    {
+      "timestamp": "2017-09-22T00:01:07.552530Z", 
+      "value": 0.213389, 
+      "value_currency": "USD", 
+      "when": "4 months ago"
+    }
+  ]
 }
 
-const elem = <h1>Witaj w JSX!</h1>;
+const TimeStamp = (data) => data.timestamp;
+const Value = (data) => data.value;
+const ValueCurrency = (data) => data.value_currency;
+const When = (data) => data.when;
 
-const course = (
+const Course = (data) => (
   <div className="wrapper">
-    <img src={data.image} />
-    <h2>{data.title}</h2>
-    <p className="desc">{data.content}</p>
-    { data.promo ? <b>Promowany</b> : null }
+    <table>
+      <tbody>
+        <tr>
+          <th>Timestamp</th>
+          <th>Value</th>
+          <th>Value currency</th>
+          <th>When</th>
+        </tr>
+        <tr>
+          <td>{ TimeStamp(data) }</td>
+          <td>{ Value(data) }</td>
+          <td>{ ValueCurrency(data) }</td>
+          <td>{ When(data) }</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 )
 
-ReactDOM.render(course,document.getElementById('root'));
+const CoursesList = (list) => list.map((data) => ( <div key={data.value}> { Course(data) } </div> ));
+const list = data.history.slice(0,6);
 
-// Komentarze JSX
-// https://eduweb.pl/player/reactjs-w-praktyce/komentarze-w-jsx
+ReactDOM.render(CoursesList(list),document.getElementById('root'));
+
+// https://eduweb.pl/player/reactjs-w-praktyce/proste-komponenty
